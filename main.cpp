6f9130804fdbfed0a3a75509dae5c39b0f7cdbb4
Paul Griffiths <mail@paulgriffiths.net>
2013-08-04 23:20:11 -0400

    Simplified main() function.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,19 @@
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-#include <cstdio>
+/*
+ *  main.cpp
+ *  ========
+ *  Copyright 2013 Paul Griffiths
+ *  Email: mail@paulgriffiths.net
+ *
+ *  Basic (very basic) usage of astronomical library.
+ *
+ *  Distributed under the terms of the GNU General Public License.
+ *  http://www.gnu.org/licenses/
+ */
 
+#include <iostream>
 #include "astro.h"
 
 int main(void) {
-
-    // Test planet
-
-    tm ct;
-    ct.tm_sec = 0;
-    ct.tm_min = 21;
-    ct.tm_hour = 19;
-    ct.tm_mday = 5;
-    ct.tm_mon = 4;
-    ct.tm_year = 80;
-
-    astro::Mars myplanet(0);
-    //astro::Mars myplanet(&ct);    //  Uncomment to set specific calc time
-
-    tm utc_time = astro::get_utc_tm(0);
-
-    std::cout << "UTC time is " << utc_time.tm_hour << ":"
-              << utc_time.tm_min << ":"
-              << utc_time.tm_sec << " according to get_utc_tm()."
-              << std::endl;
-
     astro::show_planet_positions(std::cout);
-
-    astro::Moon moon(0);
-
-    astro::OrbElem oes = moon.get_orbital_elements();
-
     return 0;
 }
